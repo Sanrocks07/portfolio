46f2aa7d3232bb3fbfb9e8b2f819a5c597f48859
Sanchit Pillai <46597284+Sanrocks07@users.noreply.github.com>
2024-03-31 22:22:47 +0530

    Update Hero.js
    Update Resume Link latest

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -18,7 +18,7 @@ const Hero = () => (
       </SectionTitle>
       <SectionText>Full Stack Dev | Programmer | Learner</SectionText>
       {/* Insert resume link here! */}
-      <Button onClick={() => window.open("https://bit.ly/3NxWGbr", "_blank")}>
+      <Button onClick={() => window.open("https://drive.google.com/file/d/1OFRXqroNHCqH4jlRrayThEbF1TLDS5CF/view?usp=sharing", "_blank")}>
         Resume
       </Button>
     </LeftSection>
